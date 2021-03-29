8fa87230ccd9e1f7d27b461994ae4876c5aa6ee7
Sidney Alcantara <sidney@sidney.me>
2021-03-29 16:02:12 +1100

    update FormFields, FieldWrapper to match form-builder

--- www/src/components/FormPreview/FieldWrapper.tsx
+++ www/src/components/FormPreview/FieldWrapper.tsx
@@ -1,17 +1,30 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import clsx from 'clsx';
 import _findIndex from 'lodash/findIndex';
 import { useDrag, useDrop } from 'react-dnd';
 import { useFiregridContext } from 'contexts/FiregridContext';
 
-import { makeStyles, createStyles, Grid, IconButton } from '@material-ui/core';
+import {
+  makeStyles,
+  createStyles,
+  useTheme,
+  Grid,
+  IconButton,
+  Checkbox,
+} from '@material-ui/core';
 import { fade } from '@material-ui/core/styles';
 import DragHandleIcon from '@material-ui/icons/DragHandle';
 import EditIcon from '@material-ui/icons/Edit';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 
 import { Friction } from '@antlerengineering/components';
-import { FieldSkeleton } from 'form-builder';
+import {
+  Field,
+  IFormFieldsProps,
+  FieldSkeleton,
+  CustomComponent,
+  getFieldProp,
+} from 'form-builder';
 import AddButton from './AddButton';
 
 const useStyles = makeStyles((theme) =>
@@ -58,24 +71,31 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
-export interface IFieldWrapperProps {
-  type: string;
-  name: string;
-  children: React.ReactNode;
+export interface IFieldWrapperProps
+  extends Field,
+    Omit<IFormFieldsProps, 'fields'> {
+  index: number;
 }
 
 export default function FieldWrapper({
+  control,
+  errors,
   type,
-  name,
-  children,
+  customComponents,
+  conditional,
+  gridCols = 12,
+  ...props
 }: IFieldWrapperProps) {
   const classes = useStyles();
+  const theme = useTheme();
+
+  const [conditionalState, setConditionalState] = useState(false);
 
   const { reOrderField } = useFiregridContext();
 
   const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
     type: 'FIELD',
-    item: { name },
+    item: { name: props.name },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
@@ -84,14 +104,72 @@ export default function FieldWrapper({
   const [{ isOver, canDrop }, drop] = useDrop(
     () => ({
       accept: 'FIELD',
-      drop: ({ name: dragName }: any) => reOrderField(dragName, name),
+      drop: ({ name: dragName }: any) => reOrderField(dragName, props.name!),
       collect: (monitor) => ({
         isOver: !!monitor.isOver(),
         canDrop: !!monitor.canDrop(),
       }),
     }),
-    [name]
+    [props.name]
   );
+
+  if (!type) {
+    console.error(`Invalid field type: ${type}`, props);
+    return null;
+  }
+
+  let renderedField: React.ReactNode = null;
+  let fieldComponent: CustomComponent;
+
+  // Try to get fieldComponent from customComponents list
+  if (
+    !!customComponents &&
+    Object.keys(customComponents).length > 0 &&
+    type in customComponents
+  ) {
+    fieldComponent = customComponents[type].component;
+  }
+  // If not found in customComponents, try to get it from the built-in components
+  else {
+    fieldComponent = getFieldProp('component', type);
+
+    // If not found in either, don’t display anything
+    if (!fieldComponent) {
+      console.error(`No matching field component for \`${type}\``);
+      return null;
+    }
+  }
+
+  if (!props.name) return null;
+
+  renderedField = React.createElement(fieldComponent, {
+    ...props,
+    name: props.name!,
+    label: props.label!,
+    control,
+    errorMessage: errors[props.name!]?.message,
+    disabled: conditional ? !conditionalState : props.disabled,
+  });
+
+  if (conditional === 'check')
+    renderedField = (
+      <Grid container wrap="nowrap" alignItems="flex-start">
+        <Grid item>
+          <Checkbox
+            checked={conditionalState}
+            onChange={(e) => {
+              setConditionalState(e.target.checked);
+              props.useFormMethods.setValue(props.name!, undefined);
+            }}
+            inputProps={{ 'aria-label': `Enable field ${props.label}` }}
+            style={{ margin: theme.spacing(1, 2, 1, -1.5) }}
+          />
+        </Grid>
+        <Grid item xs key={`${props.name}-${conditionalState}`}>
+          <Suspense fallback={<FieldSkeleton />}>{renderedField}</Suspense>
+        </Grid>
+      </Grid>
+    );
 
   return (
     <>
@@ -107,7 +185,7 @@ export default function FieldWrapper({
         <AddButton />
       </Grid>
 
-      <Grid item xs={12}>
+      <Grid item key={props.name!} id={`fieldWrapper-${props.name}`} xs={12}>
         <Grid
           container
           spacing={1}
@@ -124,7 +202,7 @@ export default function FieldWrapper({
           </Grid>
 
           <Grid item xs>
-            <Suspense fallback={<FieldSkeleton />}>{children}</Suspense>
+            <Suspense fallback={<FieldSkeleton />}>{renderedField}</Suspense>
           </Grid>
 
           <Grid item>
--- www/src/components/FormPreview/FormFields.tsx
+++ www/src/components/FormPreview/FormFields.tsx
@@ -1,42 +1,32 @@
 import React from 'react';
-import { useFormContext, useWatch, UseFormMethods } from 'react-hook-form';
 import _isFunction from 'lodash/isFunction';
+import { UseFormMethods, useWatch, useFormContext } from 'react-hook-form';
 
 import { Grid } from '@material-ui/core';
 
 import {
   Fields,
-  Field,
-  Values,
+  // Field, Values,
   CustomComponents,
-  CustomComponent,
-} from 'form-builder/types';
-import FieldWrapper from './FieldWrapper';
+} from 'form-builder';
 
-interface ICommonProps {
-  register: UseFormMethods['register'];
-  control: UseFormMethods['control'];
-  errors: UseFormMethods['errors'];
-  customComponents?: CustomComponents;
-  useFormMethods: UseFormMethods;
-}
+import FieldWrapper from './FieldWrapper'; // IFieldWrapperProps
 
 export interface IFormFieldsProps {
   fields: Fields;
+
+  // control: UseFormMethods['control'];
+  // errors: UseFormMethods['errors'];
   customComponents?: CustomComponents;
+  // useFormMethods: UseFormMethods;
 }
+
 export default function FormFields({ fields, ...props }: IFormFieldsProps) {
-  const methods = useFormContext();
-  const { register, control, errors } = methods;
+  const useFormMethods = useFormContext();
+  const { control, errors } = useFormMethods;
 
   return (
-    <Grid
-      container
-      spacing={3}
-      direction="column"
-      wrap="nowrap"
-      style={{ marginBottom: 0 }}
-    >
+    <Grid container spacing={3} style={{ marginBottom: 0 }}>
       {fields.map((field, i) => {
         // Call the field function with values if necessary
         if (_isFunction(field))
@@ -45,10 +35,9 @@ export default function FormFields({ fields, ...props }: IFormFieldsProps) {
               key={i}
               index={i}
               fieldFunction={field}
-              register={register}
               control={control}
               errors={errors}
-              useFormMethods={methods}
+              useFormMethods={useFormMethods}
               {...props}
             />
           );
@@ -58,14 +47,13 @@ export default function FormFields({ fields, ...props }: IFormFieldsProps) {
         if (!field) return null;
 
         return (
-          <FieldComponent
+          <FieldWrapper
             key={field.name ?? i}
             index={i}
             {...field}
-            register={register}
             control={control}
             errors={errors}
-            useFormMethods={methods}
+            useFormMethods={useFormMethods}
             {...props}
           />
         );
@@ -74,85 +62,16 @@ export default function FormFields({ fields, ...props }: IFormFieldsProps) {
   );
 }
 
-interface IFieldComponentProps extends Field, ICommonProps {
-  index: number;
-}
-function FieldComponent({
-  register,
-  control,
-  errors,
-  type,
-  customComponents,
-  ...fieldProps
-}: IFieldComponentProps) {
-  let renderedField: React.ReactNode = null;
-
-  switch (type) {
-    // case FieldType.heading:
-    //   renderedField = <Heading {...fieldProps} />
-    //   break
-
-    // case FieldType.description:
-    //   renderedField = <Description {...fieldProps} />
-    //   break
-
-    case undefined:
-      console.error('undefined field type');
-      return null;
-
-    default:
-      let fieldComponent: CustomComponent;
-
-      // Try to get fieldComponent from customComponents list
-      if (
-        !!customComponents &&
-        Object.keys(customComponents).length > 0 &&
-        type in customComponents
-      ) {
-        fieldComponent = customComponents[type].component;
-      }
-      // TODO:
-      // If not found in customComponents, try to get it from the built-in components
-      // else if (type in FIELD_COMPONENTS) {
-      //   fieldComponent = FIELD_COMPONENTS[type as FieldType]
-      // }
-      // If not found in either, don’t display anything
-      else {
-        // TODO: ENABLE
-        // console.error(`No matching field component for \`${type}\``)
-        return null;
-      }
-
-      if (!fieldProps.name) return null;
-
-      renderedField = React.createElement(fieldComponent, {
-        ...fieldProps,
-        name: fieldProps.name!,
-        label: fieldProps.label!,
-        control,
-        errorMessage: errors[fieldProps.name!]?.message,
-      });
-  }
-
-  return (
-    <FieldWrapper key={fieldProps.name!} name={fieldProps.name!} type={type}>
-      {renderedField}
-    </FieldWrapper>
-  );
-}
-
-interface IDependentField extends ICommonProps {
-  fieldFunction: (values: Values) => Field | null;
-  index: number;
-}
-function DependentField({ fieldFunction, ...props }: IDependentField) {
+// interface IDependentField extends Omit<IFieldWrapperProps, 'type'> {
+//   fieldFunction: (values: Values) => Field | null;
+// }
+function DependentField({ fieldFunction, ...props }: any) {
   const values = useWatch({ control: props.control });
 
   const field = fieldFunction(values);
 
   // If we intentionally hide this field due to form values, don’t render
-  // TODO:
-  if (!field) return <>FIELD HIDDEN</>;
+  if (!field) return null;
 
-  return <FieldComponent {...field} {...props} />;
+  return <FieldWrapper {...field} {...props} />;
 }
