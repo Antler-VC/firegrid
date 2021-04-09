f0b53d6f9e09304765ae6798000af146591964e9
Shams mosowi <shams.mosowi@gmail.com>
2021-04-09 12:33:43 +1000

    update createDotEnv

--- www/createDotEnv.js
+++ www/createDotEnv.js
@@ -1,12 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
 
-const main = (projectID = '', firebaseWebApiKey = '', algoliaAppId = '') => {
+const main = (
+  projectID = '',
+  firebaseWebApiKey = '',
+  algoliaAppId = '',
+  firebaseAppId = '',
+  measurementId = ''
+) => {
   return fs.writeFileSync(
     '.env',
     `REACT_APP_FIREBASE_PROJECT_ID = ${projectID}
 REACT_APP_FIREBASE_PROJECT_WEB_API_KEY = ${firebaseWebApiKey}
-REACT_APP_ALGOLIA_APP_ID = ${algoliaAppId}`
-  )
-}
+REACT_APP_ALGOLIA_APP_ID = ${algoliaAppId}
+REACT_APP_FIREBASE_APP_ID = ${firebaseAppId}
+REACT_APP_FIREBASE_MEASUREMENT_ID = ${measurementId}`
+  );
+};
 
-main(process.argv[2], process.argv[3], process.argv[4], process.argv[5])
+main(
+  process.argv[2],
+  process.argv[3],
+  process.argv[4],
+  process.argv[5],
+  process.argv[6]
+);
